db56308b2b4e4e3b228264d878bfd6744ed38a5e
Dilan Yüzlek <38807970+dilanalib@users.noreply.github.com>
2021-07-26 14:00:42 +0300

    add powershell script

---
+++ RestartServiceJenkins.ps1
@@ -0,0 +1,7 @@
+﻿$PASSWORD = ConvertTo-SecureString "$($ENV:PASSWORD)" -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ("$ENV:USERNAME", $PASSWORD)
+$service = Get-Service BITS -ErrorAction SilentlyContinue
+    if ($service.Status -eq "stopped")
+    {
+        Start-Service -Name BITS | where ($_.credential -eq  $Credential)
+    }
